a8c2bd61d8819c700fd2866eba8ae8adbb9da23d
jenaro fv <jen4r0@gmail.com>
2025-10-12 08:49:11 -0600

    grab all operation buttons on JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const sum = document.querySelector(".sum");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const division = document.querySelector(".division");
+
+const point = document.querySelector(".point");
+const erase = document.querySelector(".erase");
+const sqrt = document.querySelector(".sqrt");
+const prcnt = document.querySelector(".prcnt");
+const equals = document.querySelector(".equals");
+const neg = document.querySelector(".neg");
+const clear = document.querySelector(".clear");
